8bd4ad507967435dbd0137d0fc33835726ab4558
clandesv <47612644+clandesv@users.noreply.github.com>
2019-12-12 20:44:06 +0100

    Add files via upload

---
+++ risk-or-no-risk/game/client_types/bot.js
@@ -0,0 +1,16 @@
+/**
+ * # Bot type implementation of the game stages
+ * Copyright(c) 2019 camille <clandesvmail.uni-mannheim.de>
+ * MIT Licensed
+ *
+ * http://www.nodegame.org
+ * ---
+ */
+
+module.exports = function(treatmentName, settings, stager, setup, gameRoom) {
+
+    // Must implement the stages here.
+
+    // ...
+
+};
